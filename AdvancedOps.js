02e9cd0354cbc4865539e64d8436eeb9c76edff7
borinhoteslax <borinho.avci@gmail.com>
2020-04-17 00:36:13 +0300

    Commit Delete

--- AdvancedOps.js
+++ AdvancedOps.js
@@ -1,8 +1,22 @@
-import { LongestString } from "./LongestNumInArray.js";
-import { double } from "./smt.js";
+const LongestString = function (AV) {
+    let ResultNumber = AV[0].toString() // sets the first string as longest number
+    let ResultValue = AV[0].toString().length // sets the first string length as longest lenght 
+    let i = 0
+    while (i+1<AV.length) { // checks all numbers one by one and compares the numbers
+        if ((ResultValue)<(AV[i+1].toString().length)) { // this if statement replaces the old longest statement with the new longest statement if there is one 
+            ResultNumber = AV[i+1].toString()
+            ResultValue = AV[i+1].toString().length
+        }
+        i++
+        console.log(ResultValue , ResultNumber);
+    }
+   console.log("The longest string value in array is : " + ResultValue); // returns the longest numbers length in array
+   console.log("The longest number in array is : " + ResultNumber);  // returns the longest number in array as string
+   return ResultValue // returns function 
+}
 
 
-console.log(double(2))
+
 
 let someNum = [1, 12121 ,224234 ,13123213123 , 88888888888];
 
--- smt.js
+++
@@ -1,5 +0,0 @@
-function double(x) {
-    x = 2 * x   
-}
-
-export { double };
