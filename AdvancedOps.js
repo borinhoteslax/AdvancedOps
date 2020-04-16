0a84b462640d887b3bca1068079fe7d4882ad094
borinhoteslax <borinho.avci@gmail.com>
2020-04-17 00:49:05 +0300

    Commit Time to sleep 17.04.2020

--- AdvancedOps.js
+++ AdvancedOps.js
@@ -2,12 +2,15 @@ const LongestString = function (AV) {
     let ResultNumber = AV[0].toString() // sets the first string as longest number
     let ResultValue = AV[0].toString().length // sets the first string length as longest lenght 
     let i = 0
+    let times = 1
     while (i+1<AV.length) { // checks all numbers one by one and compares the numbers
+        console.log("The process number : " + times);
         if ((ResultValue)<(AV[i+1].toString().length)) { // this if statement replaces the old longest statement with the new longest statement if there is one 
             ResultNumber = AV[i+1].toString()
             ResultValue = AV[i+1].toString().length
         }
         i++
+        times++
         console.log(ResultValue , ResultNumber);
     }
    console.log("The longest string value in array is : " + ResultValue); // returns the longest numbers length in array
