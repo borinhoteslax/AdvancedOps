7336610fa60d8ff01f9baf2a6dfdf5bd17a9a8ae
borinhoteslax <borinho.avci@gmail.com>
2020-04-16 20:12:31 +0300

    Commit 3

---
+++ AdvancedOps.js
@@ -0,0 +1,9 @@
+import { LongestString } from "./LongestNumInArray.js";
+import { double } from "./smt.js";
+
+
+console.log(double(2))
+
+let someNum = [1, 12121 ,224234 ,13123213123 , 88888888888];
+
+LongestString(someNum);
---
+++ smt.js
@@ -0,0 +1,5 @@
+function double(x) {
+    x = 2 * x   
+}
+
+export { double };
